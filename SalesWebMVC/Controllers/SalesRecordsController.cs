0d7a0b5f57a5a13ada1acd03e7a75d4ba8912e94
George De Luca <george@deluca.eti.br>
2020-03-14 21:17:42 +0000

    Preparing sales search navigation views

---
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -0,0 +1,29 @@
+﻿// o nome do ficheiro deve ser SalesRecord+"s"+"Controller" - é o padrão do framework
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        // foi criada uma sub-pasta "SalesRecords" na pasta Views que trabalhará com este controller
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
